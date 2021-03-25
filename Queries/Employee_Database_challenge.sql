527457edfbecd0c6c126c4ae823b171e91e69bc8
Ruth Dorton <dortonruth@gmail.com>
2021-03-25 19:43:59 -0400

    Added mentor_eligibility query and csv.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -27,3 +27,21 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY ut.count DESC;
+
+-- get the current employees mentorship-eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ AND (de.to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
